0e23ac343eb1fa84f864d8c203722d08d2baee0a
minhpham1908 <31484206+minhpham1908@users.noreply.github.com>
2021-12-05 17:29:34 +0700

    Add Khabanh korne shield and keymap

---
+++ app/build.cmd
@@ -0,0 +1,2 @@
+west build --pristine -d build/khapanh_corne/left -b mikoto_520 -- -DSHIELD=khapanh_k0rn3_left
+west build --pristine -d build/khapanh_corne/right -b mikoto_520 -- -DSHIELD=khapanh_k0rn3_right
